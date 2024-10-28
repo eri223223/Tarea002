156caf33827a52cbf9e2ef1ff8ca7e84b25ea0d9
yuu0017 <yurielc025@gmail.com>
2024-10-27 23:08:46 -0300

    nav

--- views/index.php
+++ views/index.php
@@ -1,2 +1,13 @@
-<h1>Ayuda</h1>
+<header class="header">
+    <div class="logo">
+        <img src="views/src/logo.png" alt="LODZ">
+    </div>
+    <nav>
+        <ul class="links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+        </ul>
+    </nav>
+    <a href="#" class="btn"><button>Contacto</button></a>
+</header>
 <a href="<?php echo APP_URL ?>aa.php/">aa</a>
