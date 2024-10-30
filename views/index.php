718b448f14ce5ca14eafad3f8267dba47ccf3909
yuu0017 <yurielc025@gmail.com>
2024-10-30 09:27:15 -0300

    img en la parte de inicio

--- views/index.php
+++ views/index.php
@@ -1,13 +1,17 @@
-<header class="header">
-    <div class="logo">
-        <img src="views/src/logo.png" alt="LODZ">
-    </div>
-    <nav>
-        <ul class="links">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Nosotros</a></li>
-        </ul>
-    </nav>
-    <a href="#" class="btn"><button>Contacto</button></a>
-</header>
-<a href="<?php echo APP_URL ?>aa.php/">aa</a>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="views/src/logo.png" alt="LODZ">
+        </div>
+        <nav>
+            <ul class="links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Nosotros</a></li>
+            </ul>
+        </nav>
+        <a href="#" class="btn"><button>Contacto</button></a>
+    </header>
+    <a href="<?php echo APP_URL ?>aa.php/">aa</a>    
+
+</body>
+
