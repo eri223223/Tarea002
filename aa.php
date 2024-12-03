f3c16fc78e0624f5095f056d6043b3c85fd74bc7
yuu0017 <yurielc025@gmail.com>
2024-12-03 11:23:51 -0300

    inicio

--- aa.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Incluye el archivo de configuración
-require_once "config.php";
-
-// Define la vista //
-$view = "aa.php";
-
-// Incluye el archivo de diseño de la vista
-require_once "views/layout.php";
---
+++ views/register.php
@@ -0,0 +1,25 @@
+<main id="register">
+    <div class="lodz">
+        <header>
+            <h1>Regístrate</h1>
+            <p>Registra tu cuenta para seguir leyendo sobre el arte de la historia.</p>
+            <br>
+        </header>
+        <main class="registrate">
+            <form action="#">
+                <input type="text" name="username" placeholder="Nombre de Usuario" required>
+                <br><br>
+                <input type="email" name="email" placeholder="Correo Eléctrónico" required>
+                <br><br>
+                <input type="password" name="password" placeholder="Contraseña" required>
+                <br><br>
+                <button type="submit">Registrarme</button>
+            </form>
+            <a href="#">
+                <p class="estas">¿Ya Estás Registrado?</p>
+            </a>
+        </main>
+
+    </div>
+
+</main>
