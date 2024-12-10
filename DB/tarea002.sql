e90a8c015349f314e4829a870980ce75a2d4974e
eri223223 <ericaestudia223@gmail.com>
2024-12-10 11:10:02 -0300

    sume info y saque algo de css

--- DB/tarea002.sql
+++ DB/tarea002.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-12-2024 a las 17:35:17
+-- Tiempo de generación: 10-12-2024 a las 15:08:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -24,14 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pinturas-edad-media`
+-- Estructura de tabla para la tabla `pinturaem`
 --
 
-CREATE TABLE `pinturas-edad-media` (
-  `puesto` int(60) NOT NULL,
-  `pintura` varchar(60) NOT NULL,
-  `año` varchar(60) NOT NULL
+CREATE TABLE `pinturaem` (
+  `id_em` int(11) NOT NULL,
+  `Estilo` varchar(800) NOT NULL,
+  `Tiempo` varchar(800) NOT NULL,
+  `Descripcion` varchar(800) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pinturaem`
+--
+
+INSERT INTO `pinturaem` (`id_em`, `Estilo`, `Tiempo`, `Descripcion`) VALUES
+(1, 'Iconografía bizantina', 'Edad Media', 'Arte religioso con fondos dorados y colores vibrantes. Muchas obras han desaparecido.'),
+(2, 'Pintura carolingia y otoniana', 'Siglos IX al XI', 'Manuscritos iluminados con influencias bizantinas y germánicas, creados por monjes.'),
+(3, 'Pintura Románica', 'Siglos XI al XII', 'Murales y frescos en iglesias, con figuras estilizadas y temas espirituales.'),
+(4, 'Pintura Gótica', 'Siglos XII al XIV', 'Arte más naturalista y emotivo, con colores vibrantes y escenas bíblicas.'),
+(5, 'Pintura Gótica Internacional', 'Baja Edad Media', 'Estilo refinado, popular en cortes reales, con detalles intrincados y elegantes.');
 
 -- --------------------------------------------------------
 
@@ -40,11 +52,10 @@ CREATE TABLE `pinturas-edad-media` (
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(4) NOT NULL,
-  `user_name` varchar(60) NOT NULL,
+  `id_user` int(4) NOT NULL,
+  `usuario` varchar(60) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `phone` varchar(60) NOT NULL,
-  `password` varchar(60) NOT NULL
+  `clave` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -52,20 +63,20 @@ CREATE TABLE `usuarios` (
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `pinturaem`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `pinturaem`
+  ADD PRIMARY KEY (`id_em`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `pinturaem`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `pinturaem`
+  MODIFY `id_em` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
