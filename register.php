610b9ab827c342b49ff014905954b7d2aba09d19
eri223223 <ericaestudia223@gmail.com>
2024-12-10 10:04:35 -0300

    diseño login

--- register.php
+++ register.php
@@ -2,11 +2,6 @@
 // Incluye el archivo de configuración de la aplicación
 require_once "config.php";
 
-$result = mysqli_query($conn, $cadcount);
-$result2 = mysqli_query($conn, $cadgend);
-$countries = mysqli_fetch_all($result, MYSQLI_ASSOC);
-$genders = mysqli_fetch_all($result2, MYSQLI_ASSOC);
-
 // Define la vista a usar
 $view = "register.php";
 
--- views/register.php
+++ views/register.php
@@ -1,24 +1,26 @@
 <main id="register">
     <div class="lodz">
         <header>
-            <h1>Regístrate</h1>
+           
             <p>Registra tu cuenta para seguir leyendo sobre el arte de la historia.</p>
             <br>
         </header>
         <div class="registrate">
             <form action="driver_register.php" method="POST">
-                <input type="text" name="username" placeholder="Nombre de Usuario" required>
-                <br><br>
+                 <h1>Regístrate</h1>
+
+                 <div class="inpu-box">
+                    <input type="text" name="username" placeholder="Nombre de Usuario" 
+                    required>
+                 </div>
+
                 <input type="email" name="email" placeholder="Correo Eléctrónico" required>
                 <br>  
-                <input type="tel" name="phone" placeholder="Télefono" required>
-                <br>
+            
                 <input type="password" name="password" placeholder="Ingrese Una Contraseña" required>
                 <br>
-                <input type="password" name="password_repeat" placeholder="Repetir Contraseña" required>
-                <br>
-             
-                <button type="submit">Registrarme</button>
+
+                <button type="submit" value="Enviar">Registrarme</button>
             </form>
             
             <a href="login.php"><p class="estas">¿Ya Estás Registrado?</p> </a>
