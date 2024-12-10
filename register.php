10611ae39353523f45335dea4e29f23f339ab8b3
yuu0017 <yurielc025@gmail.com>
2024-12-10 12:15:00 -0300

    new-register.php-main

--- register.php
+++ register.php
@@ -2,15 +2,8 @@
 // Incluye el archivo de configuración de la aplicación
 require_once "config.php";
 
-$result = mysqli_query($conn, $cadcount);
-$result2 = mysqli_query($conn, $cadgend);
-$countries = mysqli_fetch_all($result, MYSQLI_ASSOC);
-$genders = mysqli_fetch_all($result2, MYSQLI_ASSOC);
-
 // Define la vista a usar
 $view = "register.php";
 
 // Incluye el archivo de diseño de la vista
 require_once "views/layout.php";
-
-?>
--- views/register.php
+++ views/register.php
@@ -1,23 +1,36 @@
 <main id="register">
     <div class="lodz">
         <header>
-            <h1>Regístrate</h1>
-            <p>Registra tu cuenta para seguir leyendo sobre el arte de la historia.</p>
+           
             <br>
         </header>
         <div class="registrate">
-            <form action="driver_register.php" method="POST">
-	
-                nombre de usuario: <input type="text" name="Nusu"><br>	
-                email : <input type="email" name="email"><br>	
-                clave : <input type="text" name="clave"><br>	
-                
-                <input type="submit" value="Enviar">
 
+            <form action="driver_register.php" method="POST">
+                 <h1>Regístrate</h1><br>
+                <p style="font-size:12px;">¡Registra tu cuenta para seguir sumando más historia al arte!</p>
+                 <div class="input-box">
+                    <input type="text" name="username" placeholder="Nombre de Usuario" required>
+                    <i class='bx bxs-user'></i>
+                 </div>
+
+                 <div class="input-box">
+                    <input type="email" name="email" placeholder="Correo Eléctrónico" required>
+                    <i class='bx bxs-envelope' ></i>
+                 </div>
+
+                 <div class="input-box">
+                    <input type="password" name="password" placeholder="Ingrese Una Contraseña" required>
+                    <i class='bx bxs-lock-alt' ></i>
+                 </div>
+
+                <button type="submit" class="btn">Registrarme</button>
+           
+                 <div class="register-link">
+                    <p>¿Ya tienes cuenta? <a href="login.php">Logueate</a></p>
+            </div>
             </form>
             
-            <a href="login.php"><p class="estas">¿Ya Estás Registrado?</p> </a>
-          
         </div>
 
     </div>
